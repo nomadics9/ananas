44c1b515531bb7b791763f5b5fd09659d0bac442
nomadics9 <binaskar9@gmail.com>
2024-07-04 23:45:13 +0300

    flow: fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 rootProject.name = "ananas"
 
 include(":app:phone")
-include(":app:tv")
+// include(":app:tv")
 include(":core")
 include(":data")
 include(":preferences")
