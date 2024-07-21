c293c906d45237044ff04632ab188b5c3b44f338
nomadics9 <binaskar9@gmail.com>
2024-07-21 04:38:32 +0300

    build: 15 version-0.10.5-0.14.2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 import org.gradle.api.JavaVersion
 
 object Versions {
-    const val appCode = 14
-    const val appName = "0.10.4-0.14.2"
+    const val appCode = 15
+    const val appName = "0.10.5-0.14.2"
 
     const val compileSdk = 34
     const val buildTools = "34.0.0"
